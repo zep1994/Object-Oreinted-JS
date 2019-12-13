5b0b3b745d5b1bdad1f8924facb62e1ebe5e477a
zep1994 <snaxbags@protonmail.com>
2019-12-12 22:31:47 -0500

    update script.js

--- Classes/script.js
+++ Classes/script.js
@@ -4,19 +4,20 @@
 //Instead creat a constructor function and then reuse it using the prototype
 
 //Define class suing es6
-class Vechile {
-    constructor(name, maker, model) {
-        this.name = name
-        this.maker = maker 
-        this.model = model 
-    }
+// class Vechile {
+//     constructor(name, maker, model) {
+//         this.name = name
+//         this.maker = maker 
+//         this.model = model 
+//     }
 
-    getDetails() {
-        return (`The name of the bike is ${this.name}, ${this.model}`)
-    }
-}
+//     getDetails() {
+//         return (`The name of the bike is ${this.name}, ${this.model}`)
+//     }
+// }
 
-let newVehicle1 = new Vechile("Acoord", "Honea", 2018)
+// let newVehicle1 = new Vechile("Acoord", "Honea", 2018)
 
-console.log(newVehicle1.name)
-console.log(newVehicle1.getDetails())
+// console.log(newVehicle1.name)
+// console.log(newVehicle1.getDetails())
+
