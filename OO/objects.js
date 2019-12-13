87f06721694bbe7eac1f8389d7172da9606b74e8
zep1994 <snaxbags@protonmail.com>
2019-12-12 21:55:17 -0500

    use object literall and object constructor

---
+++ OO/objects.js
@@ -0,0 +1,42 @@
+//Object
+//Class 
+//Encapsulation
+//Inheritence
+
+//Object contains properties and methods
+//Object is an instance of a class
+
+//A method is property of an object whose value is a function
+
+//Use object literall to create Object
+// let person = {
+//     fistname: "Thomas",
+//     lastname: "Matlock",
+
+//     //methog
+//     getFuntion : function() {
+//         return (`${person.fistname} ${person.lastname}`)
+//     },
+
+//     //Object within an object  
+//     phone_number : {
+//         mobile: 6018128079
+//     }
+// }
+
+
+// console.log(person.phone_number.mobile)
+// console.log(person.getFuntion())
+
+//USING AN OBJECT CONTSTRUCTOR
+function person(firstName, lastName) {
+    this.firstName = firstName
+    this.lastName = lastName
+}
+
+//creating a new instance of that object
+let person1 = new person('Tommy', 'John')
+let person2 = new person('Thomas', 'Matlock')
+
+console.log(person1.firstName)
+console.log(`${person2.firstName} ${person2.lastName}`)
